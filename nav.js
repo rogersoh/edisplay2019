91ccabf373c57cb5e76743bcdfdd1b5197e84f2e
rogersoh <jaggedfel2560@gmail.com>
2019-09-27 17:57:43 +0800

    improve nav using nav.js

---
+++ nav.js
@@ -0,0 +1,33 @@
+    document.writeln(`<a href="index.html">
+      <img src="images/marvelight-bronze-star/button-marvelight-bronze-star.jpg">
+    </a>`);
+    document.writeln(`<a href="marvelight.html">
+      <img src="images/marvelight/button-marvelight.jpg">
+    </a>`);
+    document.writeln(`<a href="racer-chronograph.html">
+      <img src="images/racer-chronograph/button-racer-chronograph.jpg">
+    </a>`);
+    document.writeln(`<a href="moon-navigator.html">
+      <img src="images/moon-navigator/button-moon-navigator.jpg">
+    </a>`);
+    document.writeln(`<a href="ceramic-midsize.html">
+      <img src="images/ceramic-midsize/button-ceramic-midsize.jpg">
+    </a>`);
+    document.writeln(`<a href="aerogmt-ii.html">
+      <img src="images/aerogmt-ii/button-aerogmt-ii.jpg">
+    </a>`);
+    document.writeln(`<a href="moon-phase.html">
+      <img src="images/moon-phase/button-moon-phase.jpg">
+    </a>`);
+    document.writeln(`<a href="aviator-oversize.html">
+      <img src="images/aviator-oversize/button-aviator-oversize-01199.jpg">
+    </a>`);
+    document.writeln(`<a href="ohio.html">
+      <img src="images/ohio/button-ohio.jpg">
+    </a>`);
+    document.writeln(`<a href="submarine-warfare-ceramic.html">
+      <img src="images/submarine-warfare-ceramic/button-submarine-warfare-ceramic.jpg">
+    </a>`);
+    document.writeln(`<a href="trainmaster-60-sec.html">
+      <img src="images/trainmaster-60-sec/button-trainmaster.jpg">
+    </a>`);
